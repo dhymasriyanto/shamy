f53a476ad16b7809bac2ecdea7d9190e8ab84268
Pizaini Pizaini <pizaini@uin-suska.ac.id>
2020-01-25 22:10:52 +0700

    update login via online server

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,23 +69,7 @@
 
 
     </div>
-    <!-- end page -->
-    <footer class="footer" style="left: 0px !important;">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    2018 - 2019 &copy; Shamy v{{config('app.version')}} by <a href="https://www.digistlab.com" target="_blank">Digistlab</a>
-                </div>
-                <div class="col-md-6">
-                    <div class="text-md-right footer-links d-none d-sm-block">
-                        <a href="javascript:void(0);">About Us</a>
-                        <a href="javascript:void(0);">Help</a>
-                        <a href="javascript:void(0);">Contact Us</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('partials.footer_large')
     <!-- Vendor js -->
     <script src="{{asset('adminto/js/vendor.min.js')}}"></script>
     {{--    <!-- App js -->--}}
--- resources/views/partials/footer_large.blade.php
+++ resources/views/partials/footer_large.blade.php
@@ -9,7 +9,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-                2018 - 2019 &copy; DigistConnect v{{config('app.version')}} by <a href="https://www.digistlab.com" target="_blank">Digistlab</a>
+                2018 - 2019 &copy; Shamy v{{config('app.version')}} by <a href="https://www.digistlab.com" target="_blank">Digistlab</a>
             </div>
             <div class="col-md-6">
                 <div class="text-md-right footer-links d-none d-sm-block">
