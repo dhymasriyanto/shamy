bc22812ed9336e3f95d0172e3be122dd6ec07bb7
Dhymas J. Riyanto <11751102195@students.uin-suska.ac.id>
2020-05-17 14:36:02 +0700

    nilai table

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,6 +11,11 @@ class ProfilesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         //
